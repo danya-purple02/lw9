1b507f82b65cae9f98bb4a9d3e67c92c3ee3a022
danya-purple02 <iza.darkwoodenn@gmail.com>
2024-11-16 11:14:55 +0300

    task 1 point 2

--- p9_1/p9_1/main.cpp
+++ p9_1/p9_1/main.cpp
@@ -1,18 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int** create_adjacency_matrix(int v);
 int cout_matrix(int** G, int v);
+void BFS_matrix(int** g, int v, int size, bool* vis, int* d);
 
-void main()
+void main() 
 {
-	int vertexes = 0;
+	int size = 0, to_start_with;
 	cout << "input graph size: ";
-	cin >> vertexes;
-	int** M = create_adjacency_matrix(vertexes);
-	cout_matrix(M, vertexes);
+	cin >> size;
+	int** M = create_adjacency_matrix(size);
+	cout_matrix(M, size);
+
+	bool *visited = new bool[size];
+	int *depth = new int[size];
+	for (int i = 0; i < size; i++) 
+	{
+		visited[i] = 0;
+		depth[i] = 0;
+	}
+	cout << endl << "input input number of vertex to star with: ";
+	cin >> to_start_with;
+	cout << endl;
+	cout << "Breadth-first matrix search: " << endl;
+	while (!visited[to_start_with])
+	{
+		BFS_matrix(M, to_start_with, size, visited, depth);
+	}
+
+	cout << endl << endl << "  Depth of vertexes: " << endl;
+	cout << "vertex		depth" << endl;
+	for (int i = 0; i < size; i++) 
+	{
+		cout << "  " << i << "		  " << depth[i] << endl;
+	}
+
+	cout << endl << "--------------------------------------------" << endl;
 	return;
 }
 
@@ -44,7 +71,7 @@ int** create_adjacency_matrix(int v)
 	return G;
 }
 
-int cout_matrix(int** G, int v)
+int cout_matrix(int** g, int v)
 {
 	cout << "    ";
 	for (int i = 0; i < v; i++)
@@ -57,9 +84,33 @@ int cout_matrix(int** G, int v)
 		cout << i << "   ";
 		for (int j = 0; j < v; j++)
 		{
-			cout << G[i][j] << " ";
+			cout << g[i][j] << " ";
 		}
 		cout << endl;
 	}
 	return 1;
+}
+
+void BFS_matrix(int** g, int v, int size, bool* vis, int* d)
+{
+	queue<int> q;
+	q.push(v);
+	vis[v] = 1;
+	d[v] = 0;
+
+	while (!q.empty()) 
+	{
+		v = q.front();
+		q.pop();
+		cout << v << " -> ";
+		for (int i = 0; i < size; i++)
+		{
+			if (g[v][i] == 1 && !vis[i]) 
+			{
+				d[i] = d[v] + 1;
+				vis[i] = 1;
+				q.push(i);
+			}
+		}
+	}
 }
